c9ad5295106c0007bf35dfafb6a6a7a92ef95f8c
ninthy <48928106+ninthy@users.noreply.github.com>
2020-09-26 12:45:03 +0300

    Update NoMoreAdsDizilla.user.js

--- NoMoreAdsDizilla.user.js
+++ NoMoreAdsDizilla.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         NoMoreAdsDizilla
-// @version      1.1
+// @version      1.3.0
 // @description  Dizilla'daki reklamları kaldırır
 // @author       Ninthy
 // @match        https://dizilla.com/*
@@ -9,7 +9,7 @@
 
 (function() {
     const elementEnum = {CLASS: 1, ID: 2, TAG: 3, SELECTOR: 4}
-
+    const log = (msg) => {console.log(`[NOADSDIZILLA] ${msg}`)};
     const elementNames = {CLASS: ["text-center xs-mb-10", "text-center xs-mb-0", "text-center xs-mt-0 xs-mb-10 lg-mb-0"], ID: ["footerFixedDiv"], TAG: ["a", "section"], SELECTOR: ["body > iframe"]}
     function deleteElement(element, type) {
         switch (type) {
@@ -17,24 +17,28 @@
                 if (document.contains(document.getElementsByClassName(element)[0])) {
                     var el = document.getElementsByClassName(element)[0];
                     el.parentNode.removeChild(el);
+                    log(`${type} deleted.`)
                 }
                 break;
             case elementEnum.ID:
                 if (document.contains(document.getElementById(element))) {
                     var el = document.getElementById(element);
                     el.parentNode.removeChild(el);
+                    log(`${type} deleted.`)
                 }
                 break;
             case elementEnum.SELECTOR:
                 if (document.contains(document.querySelector(element))) {
                     var el = document.querySelector(element);
                     el.parentNode.removeChild(el);
+                    log(`${type} deleted.`)
                 }
                 break;
             case elementEnum.TAG:
                 if (document.contains(document.getElementsByTagName(element)[0])) {
                     var el = document.getElementsByTagName(element)[0];
                     el.parentNode.removeChild(el);
+                    log(`${type} deleted.`)
                 }
                 break;
             default:
@@ -53,7 +57,7 @@
         }
 
         if (!logoAdded) {
-            document.getElementsByClassName("hoverDarker")[1].innerHTML += `<span style="color: white">NoMoreAds ${version} by ${author}</span>`;
+            document.getElementsByClassName("logo")[0].innerHTML += `<span style="color: white">NoMoreAds ${version} by ${author}</span>`;
             logoAdded = true;
         }
         for (classEl of elementNames.CLASS) {
@@ -65,12 +69,12 @@
         for (classEl of elementNames.TAG) {
             deleteElement(classEl, elementEnum.TAG);
         }
+
         for (classEl of elementNames.SELECTOR) {
             deleteElement(classEl, elementEnum.SELECTOR);
         }
-
         closeFooterFixedAd()
-        setTimeout(() => {disableAds = true; let adSkip = document.getElementById("preRollBd"); adSkip.parentNode.removeChild(adSkip); }, 250);
+        setTimeout(() => {disableAds = true; let adSkip = document.getElementById("preRollBd"); adSkip.parentNode.removeChild(adSkip); deleteElement("body > iframe", elementEnum.SELECTOR)}, 500);
     }
 
     allBypass();
